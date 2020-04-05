42bcfb07ac65e61580d0aabd49e87e7f9a8f4304
jhz7 <jhzambrano7@gmail.com>
2020-04-05 15:57:21 -0500

    Se crea pruebas unitarias a la entidad prospecto

--- src/test/scala/co/com/addi/contactbook/domain/models/ProspectTest.scala
+++ src/test/scala/co/com/addi/contactbook/domain/models/ProspectTest.scala
@@ -1,5 +1,72 @@
 package co.com.addi.contactbook.domain.models
 
-class ProspectTest {
+import akka.Done
+import co.com.addi.contactbook.TestKit
+import co.com.addi.contactbook.domain.types.BUSINESS
+import co.com.addi.contactbook.factories.PersonFactory
 
+class ProspectTest extends TestKit{
+
+  "Prospect" should {
+
+    "Validate score" when {
+
+      val firstName = "Jhon"
+      val lastName = "Zambrano"
+      val dni = PersonFactory.createDni
+      val prospect = Prospect(firstName, lastName, dni)
+
+      "The score is grater or equal the minimum allowed" must {
+        "Indicate it with a success response" in {
+          val score = 60
+          val result = prospect.validateScore(score)
+
+          result mustBe Right(Done)
+        }
+      }
+
+      "The score is below the minimum allowed" must {
+        "Indicate it with an error response" in {
+          val score = 59
+          val result = prospect.validateScore(score)
+
+          result mustBe Left(Error(BUSINESS, s"The score for the prospect ${dni.number} is below the minimum allowed"))
+        }
+      }
+    }
+
+    "Validate data equality" when {
+
+      "The data is valid" must {
+        "Indicate it with a success response" in {
+
+          val firstName = "Jhon"
+          val lastName = "Zambrano"
+          val dni = PersonFactory.createDni
+          val prospect = Prospect(firstName, lastName, dni)
+          val prospectToValidate = Prospect(firstName, lastName, dni)
+
+          val result = prospect.validateDataEquality(prospectToValidate)
+
+          result mustBe Right(Done)
+        }
+      }
+
+      "The data is valid" must {
+        "Indicate it with a success response" in {
+
+          val firstName1 = "Jhon"
+          val firstName2 = "Edwin"
+          val lastName = "Zambrano"
+          val dni = PersonFactory.createDni
+          val prospect = Prospect(firstName1, lastName, dni)
+          val prospectToValidate = Prospect(firstName2, lastName, dni)
+
+          val result = prospect.validateDataEquality(prospectToValidate)
+
+          result mustBe Left(Error(BUSINESS, s"The 'First Name' value is not valid for prospect ${dni.number}"))
+        }
+      }
+    }
+  }
 }
